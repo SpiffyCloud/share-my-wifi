bd2a701544bfbd9aba9e9d81558d170b24708adf
Dayron Edmundo Acosta <dayroned@gmail.com>
2022-09-23 23:55:01 -0400

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed, onBeforeMount } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { Preferences } from '@capacitor/preferences';
 import Logo from './components/Logo.vue';
 import Card from './components/Card.vue';
@@ -13,7 +13,7 @@ import LayoutSelected from './components/LayoutSelected.vue';
 
 // data related
 const dataLoaded = ref(false)
-const credentials = ref({ name: '', password: '', updated: null })
+const credentials = ref(null)
 const showForm = ref(null)
 
 onMounted(() => {
@@ -24,8 +24,10 @@ async function loadCredentials() {
   const { value } = await Preferences.get({ key: 'credentials' })
   if (value) {
     credentials.value = JSON.parse(value)
+  } else {
+    credentials.value = { name: '', password: '', saved: 'no' }
+    showForm.value = true
   }
-  showForm.value = !hasCredentials()
   dataLoaded.value = true
 }
 
@@ -37,98 +39,75 @@ async function deleteCredentials() {
   await Preferences.remove({ key: 'credentials' })
 }
 
-function hasCredentials() {
-  return credentials.value.name !== ''
-}
-
 function hideForm() {
-  // determine if timestamp should be updated
-  if (true) {
-    const date = new Date()
-    credentials.value.updated = date.toLocaleString('en', {
-      dateStyle: 'medium',
-      timeStyle: 'short'
-    })
-    saveCredentials()
-  }
+  const date = new Date()
+  saveCredentials()
+  credentials.value.saved = 'yes'
   showForm.value = false
-  // store new credential values
-  // currentCredentials.name = credentials.value.name
-  // currentCredentials.password = credentials.value.password
-}
-
-function isValidSSID(str) {
-  return /^[^!#;+\]/"\t][^+\]/"\t]{1,31}$/.test(str)
 }
 
 function resetStatus() {
   deleteCredentials()
-  credentials.value = { name: '', password: '', updated: null }
+  credentials.value = { name: '', password: '', saved: 'no' }
   showForm.value = true
 }
 
+function isValidSSID(input) {
+  return /^[^!#;+\]/"\t][^+\]/"\t]{1,31}$/.test(input)
+}
+
+function isValidPassword(input) {
+  return input >= 4
+}
+
+const noCredentials = computed(() => {
+  return credentials.value.saved === 'no'
+})
+
+const title = computed(() => {
+  return noCredentials.value ? '' : credentials.value.name
+})
+
 const instructions = computed(() => {
   if (showForm.value) {
-    return `${hasCredentials() ? 'Update' : 'Add'} your wifi credentials ${hasCredentials() ? '' : 'for easy sharing'}`
+    return `${noCredentials.value ? 'Add' : 'Update'} your wifi credentials ${noCredentials.value ? 'for easy sharing' : ''}`
   } else {
     return 'Share your wifi with others by having them scan this QR code'
   }
 })
 
-const qr = computed(() => {
-  if (hasCredentials) {
-    const { name, password } = credentials.value
-    return `WIFI:S:${name};;P:${password};;`
-  } else return ''
+const invalidCredentials = computed(() => {
+  // TODO: error messages
+  return !(isValidSSID(credentials.value.name) && isValidPassword(credentials.value.password.length))
 })
 
-// const credentialsUpdated = computed(() => {
-//   if (currentCredentials.name !== credentials.value.name ||
-//     currentCredentials.password !== credentials.value.password) {
-//     return true
-//   } else return false
-// })
-
-// TODO: error messages
-const areCredentialsValid = computed(() => {
+const qr = computed(() => {
   const { name, password } = credentials.value
-  return (isValidSSID(name) && password.length > 4)
+  return `WIFI:T:WPA;S:${name};P:${password};;`
 })
 </script>
 
 <template>
   <Logo />
-  <LayoutSelected v-if="dataLoaded"
-    :title="credentials.name" :instructions="instructions"
-    :showTimestamp="!showForm"
-    :timestamp="credentials.updated">
+  <LayoutSelected v-if="dataLoaded" :title="title" :instructions="instructions">
     <template v-slot:card>
       <Card :back="!showForm" @click="showForm = true">
         <template v-slot:front>
-          <InputField :group="'name'" :type="'text'"
-            :placeholder="'My WiFi'"
-            v-model="credentials.name" />
-          <InputField :group="'password'" :type="'text'"
-            :placeholder="'password123'"
-            v-model="credentials.password" />
+          <InputField :group="'name'" :type="'text'" :placeholder="'My WiFi'" v-model="credentials.name" />
+          <InputField :group="'password'" :type="'text'" :placeholder="'password123'" v-model="credentials.password" />
         </template>
         <template v-slot:back>
-          <qrCode v-if="hasCredentials" :value="qr"
-            :render-as="'svg'" :margin="0"
-            :background="'none'"
-            :class="{ foreground: !showForm, none: showForm }" />
+          <qrCode :value="qr" :render-as="'svg'" :margin="0" :background="'none'" class="foreground" />
         </template>
       </Card>
     </template>
     <template v-slot:button>
-      <ActionButton v-if="showForm" :type="'submit'"
-        :disabled="!areCredentialsValid" @click="hideForm">
-        <IconCheck v-if="hasCredentials" />
-        <IconPlus v-else />
-        <span>{{ hasCredentials ? 'Done' : 'Add' }}</span>
+      <ActionButton v-if="showForm" :type="'submit'" :disabled="invalidCredentials" @click="hideForm">
+        <IconPlus v-if="noCredentials" />
+        <IconCheck v-else />
+        <span>{{ noCredentials ? 'Add' : 'Done' }}</span>
       </ActionButton>
-      <ActionButton v-else :type="'delete'"
-        @click="resetStatus">
+      <ActionButton v-else :type="'delete'" @click="resetStatus">
         <IconTrash />
         <span>Delete</span>
       </ActionButton>
--- src/components/LayoutSelected.vue
+++ src/components/LayoutSelected.vue
@@ -5,34 +5,19 @@ import TextTitle from './TextTitle.vue';
 const props = defineProps([
   'title',
   'instructions',
-  'timestamp',
-  'showTimestamp'
 ])
 </script>
 
 <template>
   <Transition mode="out-in">
-    <TextInstructions :text="instructions"
-      :key="instructions" />
+    <TextInstructions :text="instructions" :key="instructions" />
   </Transition>
   <TextTitle :text="title" />
   <slot name="card"></slot>
-  <p class="timestamp">
-    <Transition>
-      <span v-if="showTimestamp">Last Updated: {{ timestamp
-      }}</span>
-    </Transition>
-  </p>
   <slot name="button"></slot>
 </template>
 
 <style>
-.timestamp {
-  font-size: var(--font-size-tiny);
-  margin: 0;
-  height: 1rem;
-}
-
 .v-enter-active,
 .v-leave-active {
   transition-delay: .25s;
